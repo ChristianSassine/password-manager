a680efc86c4b86c35a5529d0628d274d0c9a490d
Christian Sassine <71024958+ChristianSassine@users.noreply.github.com>
2023-07-04 23:33:18 -0400

    feat: add hashing for users' password

---
+++ server/internal/hashing/hash_password.go
@@ -0,0 +1,14 @@
+package hashing
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	bcryptCost := 10
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	return string(bytes), err
+}
+
+func CheckPassword(password, encryptedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(encryptedPassword), []byte(password))
+	return err == nil
+}
