b2203dc2cbeadc60371eaa0a986652e0b1b2eeac
Christian Sassine <71024958+ChristianSassine@users.noreply.github.com>
2023-07-09 18:45:16 -0400

    setup: create template and utils for commands

--- pass-cli/cmd/root.go
+++ pass-cli/cmd/root.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,18 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "client",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "pass-cli",
+	Short: "CLI to manage passwords",
+	Long: `pass-cli is a CLI that communicates with the password manager server.
+	It allows to quickly manage and read passwords from the client.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -47,5 +40,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
--- pass-cli/main.go
+++ pass-cli/main.go
@@ -3,7 +3,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/ChristianSassine/password-manager/pass-cli/cmd"
+import (
+	"github.com/ChristianSassine/password-manager/pass-cli/cmd"
+)
 
 func main() {
 	cmd.Execute()
