98cede040a1eb6d31b202e5df64d373f001f8ac7
Christian Sassine <71024958+ChristianSassine@users.noreply.github.com>
2023-07-09 22:10:14 -0400

    feat: add get command functionality

--- pass-cli/cmd/get.go
+++ pass-cli/cmd/get.go
@@ -1,27 +1,25 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/ChristianSassine/password-manager/pass-cli/internal/manager"
 	"github.com/spf13/cobra"
 )
 
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Retrieves the password that matchs the key",
+	Long: `This command sends a request to the server to retrieve the password matching with the key.
+	It will take the key as argument.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		// TODO: Handle flags
+		if len(args) == 0 {
+			cmd.Help()
+		}
+		manager.GetPassword(args[0])
 	},
 }
 
--- pass-cli/go.mod
+++ pass-cli/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require github.com/spf13/cobra v1.7.0
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/fatih/color v1.15.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
